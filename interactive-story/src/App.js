553be326990c685f613a1b112004a58aa4cfc36c
Stacy Layton <stacy.layton@familysearch.org>
2018-12-18 14:11:14 -0700

    Adding deploy information. Added alt tags to the instruments.

--- interactive-story/src/App.js
+++ interactive-story/src/App.js
@@ -4,10 +4,7 @@ import './App.css';
 
 class App extends Component {
   playInstrument(e) {
-    console.log(e.target);
     var prettySound = new Audio(e.target.dataset.sound);
-    console.log(e.target.dataset.sound);
-    console.log(prettySound);
     prettySound.play();
   }
 
@@ -23,24 +20,42 @@ class App extends Component {
           </div>
 
           <div className="scrolling-wrapper" onClick={this.playInstrument}>
-            <img src="pics/Instruments/TN_accordion.jpg" data-sound="music/electric-guitar1.wav" id="accordion" className="input" />
-            <img src="pics/Instruments/TN_acoustic_guitar.jpg" data-sound="music/flute1.wav" className="input" />
-            <img src="pics/Instruments/TN_bagpipe.jpg" data-sound="music/xylophone1.wav" className="input" />
-            <img src="pics/Instruments/TN_banjo.jpg" data-sound="music/electric-guitar1.wav" className="input" />
-            <img src="pics/Instruments/TN_cello.jpg" data-sound="music/electric-guitar1.wav" className="input" />
-            <img src="pics/Instruments/TN_clarinet.jpg" className="input" />
-            <img src="pics/Instruments/TN_drum.jpg" className="input" />
-            <img src="pics/Instruments/TN_french_horn.jpg" className="input" />
-            <img src="pics/Instruments/TN_harp.jpg" className="input" />
-            <img src="pics/Instruments/TN_keyboard.jpg" className="input" />
-            <img src="pics/Instruments/TN_red-color-electric-guitar.jpg" className="input" />
-            <img src="pics/Instruments/TN_saxophone.jpg" className="input" />
-            <img src="pics/Instruments/TN_trombone.jpg" className="input" />
-            <img src="pics/Instruments/TN_trumpet.jpg" className="input" />
-            <img src="pics/Instruments/TN_tuba.jpg" className="input" />
-            <img src="pics/Instruments/TN_viola.jpg" className="input" />
-            <img src="pics/Instruments/TN_violin.jpg" className="input" />
-            <img src="pics/Instruments/TN_xylophone.jpg" className="input" />
+            <img src="pics/Instruments/TN_accordion.jpg" data-sound="" 
+            alt="accordion" className="input" />
+            <img src="pics/Instruments/TN_acoustic_guitar.jpg" data-sound=" " 
+            alt="acoustic_guitar" className="input" />
+            <img src="pics/Instruments/TN_bagpipe.jpg" data-sound=" " 
+            alt="acoustic_guitar"className="input" />
+            <img src="pics/Instruments/TN_banjo.jpg" data-sound="music/banjo.mp3" 
+            alt="acoustic_guitar"className="input" />
+            <img src="pics/Instruments/TN_cello.jpg" data-sound=" " 
+            alt="cello" className="input" />
+            <img src="pics/Instruments/TN_clarinet.jpg" data-sound="music/clarinet.mp3" 
+            alt="clarinet" className="input" />
+            <img src="pics/Instruments/TN_drum.jpg" data-sound="music/snare-drum.mp3"
+            alt="drum" className="input" />
+            <img src="pics/Instruments/TN_french_horn.jpg" 
+            alt="french_horn" className="input" />
+            <img src="pics/Instruments/TN_harp.jpg" 
+            alt="harp" className="input" />
+            <img src="pics/Instruments/TN_keyboard.jpg" 
+            alt="keyboard" className="input" />
+            <img src="pics/Instruments/TN_red-color-electric-guitar.jpg" 
+            alt="electric-guitar" className="input" />
+            <img src="pics/Instruments/TN_saxophone.jpg" 
+            alt="saxophone" className="input" />
+            <img src="pics/Instruments/TN_trombone.jpg" 
+            alt="trombone" className="input" />
+            <img src="pics/Instruments/TN_trumpet.jpg" 
+            alt="trumpet" className="input" />
+            <img src="pics/Instruments/TN_tuba.jpg" 
+            alt="tuba" className="input" />
+            <img src="pics/Instruments/TN_viola.jpg" 
+            alt="viola" className="input" />
+            <img src="pics/Instruments/TN_violin.jpg" 
+            alt="violin" className="input" />
+            <img src="pics/Instruments/TN_xylophone.jpg" 
+            alt="xylophone" className="input" />
           </div>
       </div>
 
